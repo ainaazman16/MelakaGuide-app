f1a78740f37efcaee20360d6bd8592ab6c005a8c
Aina Maisarah Azman <amaisarah160@gmail.com>
2025-09-04 10:37:25 +0800

    topnav

--- resources/views/layouts/topnav.blade.php
+++ resources/views/layouts/topnav.blade.php
@@ -1,73 +1,56 @@
 @inject('layoutHelper', 'JeroenNoten\LaravelAdminLte\Helpers\LayoutHelper')
 
-<nav
-    class="main-header navbar
-    {{ config('adminlte.classes_topnav_nav', 'navbar-expand-md') }}
+<nav class="main-header navbar
+    {{ config('adminlte.classes_topnav_nav', 'navbar-expand') }}
     {{ config('adminlte.classes_topnav', 'navbar-white navbar-light') }}">
 
-    <div class="{{ config('adminlte.classes_topnav_container', 'container') }}">
+    {{-- Navbar left links --}}
+    <ul class="navbar-nav">
+        {{-- Left sidebar toggler link --}}
+        @include('adminlte::partials.navbar.menu-item-left-sidebar-toggler')
 
-        {{-- Navbar brand logo --}}
-        @if (config('adminlte.logo_img_xl'))
-            @include('adminlte::partials.common.brand-logo-xl')
+        {{-- Configured left links --}}
+        @each('adminlte::partials.navbar.menu-item', $adminlte->menu('navbar-left'), 'item')
+
+        {{-- Custom left links --}}
+        @yield('content_top_nav_left')
+    </ul>
+
+    {{-- Navbar right links --}}
+   {{-- Navbar right links --}}
+<ul class="navbar-nav ml-auto order-1 order-md-3 navbar-no-expand">
+    {{-- Custom right links --}}
+    @yield('content_top_nav_right')
+
+    {{-- Configured right links --}}
+    @each('adminlte::partials.navbar.menu-item', $adminlte->menu('navbar-right'), 'item')
+
+    {{-- Auth Links --}}
+    @guest
+         <li class="nav-item">
+             <a href="{{ route('places.index') }}" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('login') }}" class="nav-link">Login</a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('register') }}" class="nav-link">Register</a>
+        </li>
+    @endguest
+
+    @auth
+        @if(config('adminlte.usermenu_enabled'))
+            @include('adminlte::partials.navbar.menu-item-dropdown-user-menu')
         @else
-            @include('adminlte::partials.common.brand-logo-xs')
+            @include('adminlte::partials.navbar.menu-item-logout-link')
         @endif
+    @endauth
 
-        {{-- Navbar toggler button --}}
-        <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse"
-            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    {{-- Right sidebar toggler link --}}
+    @if($layoutHelper->isRightSidebarEnabled())
+        @include('adminlte::partials.navbar.menu-item-right-sidebar-toggler')
+    @endif
+</ul>
 
-        {{-- Navbar collapsible menu --}}
-        <div class="collapse navbar-collapse order-3" id="navbarCollapse">
-            {{-- Navbar left links --}}
-            <ul class="nav navbar-nav">
-                {{-- Configured left links --}}
-                @each('adminlte::partials.navbar.menu-item', $adminlte->menu('navbar-left'), 'item')
-
-                {{-- Custom left links --}}
-                @yield('content_top_nav_left')
-            </ul>
-        </div>
-
-        {{-- Navbar right links --}}
-        <ul class="navbar-nav ml-auto order-1 order-md-3 navbar-no-expand">
-            {{-- Custom right links --}}
-            @yield('content_top_nav_right')
-
-            {{-- Configured right links --}}
-            @each('adminlte::partials.navbar.menu-item', $adminlte->menu('navbar-right'), 'item')
-
-            {{-- Auth Links --}}
-            @guest
-                <li class="nav-item">
-                    <a href="{{ route('places.index') }}" class="nav-link">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('login') }}" class="nav-link">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('register') }}" class="nav-link">Register</a>
-                </li>
-            @endguest
-
-            @auth
-                @if (config('adminlte.usermenu_enabled'))
-                    @include('adminlte::partials.navbar.menu-item-dropdown-user-menu')
-                @else
-                    @include('adminlte::partials.navbar.menu-item-logout-link')
-                @endif
-            @endauth
-
-            {{-- Right sidebar toggler link --}}
-            @if ($layoutHelper->isRightSidebarEnabled())
-                @include('adminlte::partials.navbar.menu-item-right-sidebar-toggler')
-            @endif
-        </ul>
-
-
-    </div>
 
 </nav>
